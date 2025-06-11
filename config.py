64697f431260b26418a19acd0b3409d569e6b483
Jeevanpranav-23 <jeevanpranav23@gmail.com>
2025-06-11 19:10:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 # todo: Add your api key here
-apikey = "Your-Open-AI-Key"
+apikey = "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"
